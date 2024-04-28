f47bc8d04cccc51f339902542b257ce68c5997ab
Fash Muyhee <fashmuyidavid@gmail.com>
2024-04-29 00:54:54 +0100

    add drop shadow to tabbar

--- src/routes/components/TabBar.tsx
+++ src/routes/components/TabBar.tsx
@@ -7,7 +7,21 @@ export const TabBar = ({state, descriptors, navigation}: BottomTabBarProps) => {
   return (
     <StackView
       justify="space-between"
-      style={{paddingBottom: IS_ANDROID ? 0 : 10, height: 80, width: '100%', backgroundColor: COLORS.WHITE, paddingHorizontal: 30}}>
+      style={{
+        paddingBottom: IS_ANDROID ? 0 : 10,
+        height: 80,
+        width: '100%',
+        backgroundColor: COLORS.WHITE,
+        paddingHorizontal: 30,
+        shadowColor: COLORS.GREY,
+        shadowOffset: {
+          width: 0,
+          height: 4,
+        },
+        shadowOpacity: 0.4,
+        shadowRadius: 4.65,
+        elevation: 8,
+      }}>
       {state.routes.map((route, index) => {
         const {options} = descriptors[route.key];
         const label = options.tabBarLabel as string;
