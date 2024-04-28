750b7ebfa1a71fff8f323cbe9818681d56283ecb
Fash Muyhee <fashmuyidavid@gmail.com>
2024-04-29 00:55:01 +0100

    restyled

--- src/components/commons/ScreenWrapper.tsx
+++ src/components/commons/ScreenWrapper.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Edge, SafeAreaView} from 'react-native-safe-area-context';
 import {ScreenWrapperProps} from './types';
-import {COLORS, SCREEN_PADDING} from '@utils';
+import {COLORS, SCREEN_HEIGHT, SCREEN_PADDING} from '@utils';
 
 export const ScreenWrapper = ({children, bg, padding = SCREEN_PADDING, noEdges = false, style}: ScreenWrapperProps) => {
   bg = bg ? bg : COLORS.OFF_WHITE;
 
   const edges: Edge[] = React.useMemo(() => {
-    return noEdges ? ['bottom'] : ['bottom', 'top'];
+    return noEdges ? [] : ['bottom', 'top'];
   }, [noEdges]);
 
   return (
-    <SafeAreaView style={[{backgroundColor: bg, paddingHorizontal: padding, flex: 1}, style]} edges={edges}>
+    <SafeAreaView style={[{backgroundColor: bg, paddingHorizontal: padding, height: SCREEN_HEIGHT}, style]} edges={edges}>
       {children}
     </SafeAreaView>
   );
