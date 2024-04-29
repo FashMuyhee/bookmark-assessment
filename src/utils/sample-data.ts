b0e02a8d05a84f04649b73467cf60f91664b4139
Fash Muyhee <fashmuyidavid@gmail.com>
2024-04-29 01:04:22 +0100

    add sample data for categories section

---
+++ src/utils/sample-data.ts
@@ -0,0 +1,12 @@
+import {BroomIcon, HairSalonIcon, MechToolIcon, MoversIcon, PaintBrushIcon, PlumbingIcon, RepairToolIcon, WasherIcon} from '@components';
+
+export const categories = [
+  {category: 'cleaning', icon: BroomIcon},
+  {category: 'repairing', icon: RepairToolIcon},
+  {category: 'painting', icon: PaintBrushIcon},
+  {category: 'laundry', icon: WasherIcon},
+  {category: 'appliance', icon: MechToolIcon},
+  {category: 'plumbing', icon: PlumbingIcon},
+  {category: 'movers', icon: MoversIcon},
+  {category: "men's salon", icon: HairSalonIcon},
+];
