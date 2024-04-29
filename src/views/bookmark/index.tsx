75797c0ea54b9c1cf0ac4c20ff5a406f8de30882
Fash Muyhee <fashmuyidavid@gmail.com>
2024-04-29 14:51:59 +0100

    add remove animation

--- src/views/bookmark/index.tsx
+++ src/views/bookmark/index.tsx
@@ -3,7 +3,7 @@ import {ScreenWrapper} from '@components';
 import {COLORS, SCREEN_HEIGHT, SCREEN_PADDING, Service} from '@utils';
 import {useBookmark} from '@hooks';
 import {ServiceCard, ServiceCategoryList} from '@views/home/component';
-import {FlatList, ListRenderItemInfo} from 'react-native';
+import {FlatList, LayoutAnimation, ListRenderItemInfo} from 'react-native';
 import {Header} from './components';
 
 type Props = {};
@@ -12,7 +12,10 @@ export const BookmarkScreen = (props: Props) => {
   const {bookmarks, onToggleBookmark} = useBookmark();
   const [category, setCategory] = React.useState('all');
 
-  const renderBookmarks = ({item}: ListRenderItemInfo<Service>) => <ServiceCard {...item} onToggleBookmark={onToggleBookmark} />;
+  const renderBookmarks = ({item}: ListRenderItemInfo<Service>) => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.linear);
+    return <ServiceCard {...item} onToggleBookmark={onToggleBookmark} />;
+  };
 
   return (
     <ScreenWrapper padding={0} bg={COLORS.WHITE} style={{height: SCREEN_HEIGHT - 50}}>
